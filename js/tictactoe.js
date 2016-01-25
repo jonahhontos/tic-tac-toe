82a0d34c16fa5f5d88b2d28fc2bd701c48976e6a
Jonah Hontos <jonahhontos@gmail.com>
2016-01-25 09:03:59 -0800

    first commit

---
+++ js/tictactoe.js
@@ -0,0 +1,45 @@
+var turn = false
+var spots = document.body.querySelectorAll(".wrap div")
+
+for (var i=0; i<spots.length; i+=1) {
+  spots[i].addEventListener("click", function(){
+    clickSpot(this)
+  })
+}
+
+function reset() {
+  for (var i=0; i<spots.length; i+=1) {
+    spots[i].innerText = ""
+    spots[i].className = ""
+  }
+}
+
+function clickSpot(spot) {
+  if (spot.innerText===""){
+    if (turn) {
+      spot.innerText = "X"
+      spot.className = "red"
+    } else {
+      spot.innerText = "O"
+      spot.className = "blue"
+    }
+    checkForWin()
+    turn = !turn
+  }
+}
+
+function checkForWin() {
+  var p = turn ? "X" : "O"
+  // console.log(p + spots[0].innerText)
+  if ((spots[0].innerText=== p && spots[1].innerText === p && spots[2].innerText === p) ||
+      (spots[0].innerText=== p && spots[3].innerText === p && spots[6].innerText === p) ||
+      (spots[0].innerText=== p && spots[4].innerText === p && spots[8].innerText === p) ||
+      (spots[1].innerText=== p && spots[4].innerText === p && spots[7].innerText === p) ||
+      (spots[2].innerText=== p && spots[4].innerText === p && spots[6].innerText === p) ||
+      (spots[2].innerText=== p && spots[5].innerText === p && spots[8].innerText === p) ||
+      (spots[3].innerText=== p && spots[4].innerText === p && spots[5].innerText === p) ||
+      (spots[6].innerText=== p && spots[7].innerText === p && spots[8].innerText === p)) {
+        alert(p + " wins!")
+        reset()
+      }
+}
